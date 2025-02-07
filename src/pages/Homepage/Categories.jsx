43b66e1f7cb19ce09d30cb339ca357f309a36db9
hrishita31 <hrishita.3134@gmail.com>
2025-02-07 10:14:19 +0530

    updated code for getting products

--- src/pages/Homepage/Categories.jsx
+++ src/pages/Homepage/Categories.jsx
@@ -1,7 +1,26 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+// import getData from "../../requests/getRequest";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const Categories = () => {
+
+  const url = import.meta.env.VITE_API_URL;
+
+
+  const getCategory = async (category) => {
+    try {
+      const { data } = await axios.get(`${url}/products/getProduct`, {
+        params: { category },
+      });
+      console.log(data, 345);
+      console.log("Desserts present:" ,data.result)
+    } catch (error) {
+      toast.error(`Error fetching the category: ${category}. ${error.message}`);
+    }
+  };
+  
   const responsive = {
     desktop: {
       breakpoint: { max: 2000, min: 1024 },
@@ -25,6 +44,11 @@ export const Categories = () => {
       <div className="categories">
         <div className="container">
           <div className="row">
+
+          <div className="section-title">
+                <span>Our specialties</span>
+                <h2>A must try for you and your loved ones! </h2>
+              </div>
             <Carousel
               additionalTransfrom={0}
               arrows
@@ -53,40 +77,35 @@ export const Categories = () => {
               slidesToSlide={2}
               swipeable
             >
+
               <div className="categories__item">
-                <div className="categories__item__icon">
+                <div className="categories__item__icon" onClick={() => getCategory("cupcake")}>
                   <span className="flaticon-029-cupcake-3"></span>
                   <h5>Cupcake</h5>
                 </div>
               </div>
               <div className="categories__item">
-                <div className="categories__item__icon">
+                <div className="categories__item__icon" onClick={() => getCategory("cake")}>
                   <span className="flaticon-034-chocolate-roll"></span>
-                  <h5>Butter</h5>
+                  <h5>Cake</h5>
                 </div>
               </div>
               <div className="categories__item">
-                <div className="categories__item__icon">
+                <div className="categories__item__icon" onClick={() => getCategory("cheesecake")}>
                   <span className="flaticon-005-pancake"></span>
-                  <h5>Red Velvet</h5>
+                  <h5>Cheesecake</h5>
                 </div>
               </div>
               <div className="categories__item">
-                <div className="categories__item__icon">
+                <div className="categories__item__icon" onClick={() => getCategory("brownie")}>
                   <span className="flaticon-030-cupcake-2"></span>
-                  <h5>Biscuit</h5>
+                  <h5>Brownie</h5>
                 </div>
               </div>
               <div className="categories__item">
-                <div className="categories__item__icon">
+                <div className="categories__item__icon" onClick={() => getCategory("donut")}>
                   <span className="flaticon-006-macarons"></span>
                   <h5>Donut</h5>
-                </div>
-              </div>
-              <div className="categories__item">
-                <div className="categories__item__icon">
-                  <span className="flaticon-006-macarons"></span>
-                  <h5>Cupcake</h5>
                 </div>
               </div>
             </Carousel>
